2951b8154568071e3beef926a8f03b614187d577
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2020-11-24 19:37:15 -0700

    add inquirer and fs functions

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
-const questions = require('utils/questions');
+const questions = require("./utils/questions.js");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+inquirer.prompt(questions).then((input) => {
+  const {
+    title,
+    description,
+    installation,
+    usage,
+    contribution,
+    test,
+    license,
+    github,
+    email,
+  } = input;
 
-// function to initialize program
-function init() {
+  const mdTemplate = `# ${title}
 
-}
+![License](https://img.shields.io/badge/License-${license}-blue)
 
-// function call to initialize program
-init();
+## Description 
+${description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [License](#license)
+* [Questions](#questions)
+
+
+## Installation
+${installation}
+
+
+## Usage 
+${usage}
+
+## Contributing
+${contribution}
+
+## Tests
+${test}
+
+## License
+${license}
+
+## Questions
+GitHub username: ${github}
+
+email: ${email}`;
+
+  fs.writeFile("README.md", mdTemplate, () => {
+    console.log("README created!");
+  });
+});
+
+// // function to write README file
+// function writeToFile(fileName, data) {}
+
+// // function to initialize program
+// function init() {}
+
+// // function call to initialize program
+// init();
